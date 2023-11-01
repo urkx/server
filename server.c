76372aa95cba8162fd00a65145b75d7c461c7322
Urko Lopez <urkolopezlopez@gmail.com>
2023-11-01 12:43:01 +0100

    Resources handled

--- server.c
+++ server.c
@@ -1,15 +1,4 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <stdbool.h>
-#include <arpa/inet.h>
-
-#define PORT 8080
+#include "server.h"
 
 void error(char *msg) {
     perror(msg);
@@ -49,7 +38,42 @@ bool addHeader(int *socket, char *header) {
     return res;
 }
 
-void handleConnections(int *create_socket, struct sockaddr_in *address, char *buffer, int bufsize, long fsize, char *msg) {
+struct Resource getResource(char *buffer) {
+    char *fh = strtok(buffer, "\r\n");
+    strtok(fh, " ");
+    char *resource = strtok(NULL, " ");
+    struct Resource res;
+
+    if(strcmp(resource, root) == 0) resource = "/index.html";
+
+    resource = resource + 1;
+    printf("%s", resource);
+
+    FILE *fp = fopen(resource, "rb");
+    if(!fp) error("File not opened");
+
+    printf("File opened\n");
+
+    if(fseek(fp, 0, SEEK_END) == -1) error("File not seeked");
+
+    long fsize = ftell(fp);
+    if(fsize == -1) error("File size not retrieved");
+
+    rewind(fp);
+
+    char *msg = (char *)malloc(fsize);
+    if(!msg) error("File buffer not allocated");
+
+    if(fread(msg, fsize, 1, fp) != 1) error("File not read");
+
+    fclose(fp);
+
+    res.fsize = fsize;
+    res.resource = msg;
+    return res;
+}
+
+void handleConnections(int *create_socket, struct sockaddr_in *address, char *buffer, int bufsize) {
     socklen_t addrlen;
     int new_socket;
     while(1) {
@@ -61,6 +85,7 @@ void handleConnections(int *create_socket, struct sockaddr_in *address, char *bu
         printf("Client connected from %s:%hu\n", inet_ntoa(address->sin_addr), ntohs(address->sin_port));
 
         int numRead = recv(new_socket, buffer, bufsize, 0);
+
         if(numRead < 1) {
             if(numRead == 0) {
                 printf("Client disconnected\n");
@@ -72,10 +97,13 @@ void handleConnections(int *create_socket, struct sockaddr_in *address, char *bu
         }
         printf("%.*s\n", numRead, buffer);
 
+        struct Resource resource = getResource(buffer);
+
+
         if(!addHeader(&new_socket, "HTTP/1.1 200 OK\r\n")) continue;
 
         char clen[40];
-        sprintf(clen, "Content-length: %ld\r\n", fsize);
+        sprintf(clen, "Content-length: %ld\r\n", resource.fsize);
 
         if(!addHeader(&new_socket, clen)) continue;
 
@@ -83,7 +111,7 @@ void handleConnections(int *create_socket, struct sockaddr_in *address, char *bu
 
         if(!addHeader(&new_socket, "Connection: close\r\n\r\n")) continue;
 
-        if(!writeDataToClient(new_socket, msg, fsize)) {
+        if(!writeDataToClient(new_socket, resource.resource, resource.fsize)) {
             close(new_socket);
             continue;
         }
@@ -139,7 +167,7 @@ int main() {
 
     printf("Socket is listening\n");
 
-    handleConnections(&create_socket, &address, buffer, bufsize, fsize, msg);
+    handleConnections(&create_socket, &address, buffer, bufsize);
 
     close(create_socket);
     return 0;
--- server.h
+++ server.h
@@ -1,10 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
 
-struct server
-{
-    sockaddr_in addr;
-    int addr_len;
-    int port;
+#define PORT 8080
 
+static char *root = {"/"};
+
+struct Resource {
+    char *resource;
+    long fsize;
 };
